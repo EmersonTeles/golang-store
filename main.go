67c951dd031a0f8917b433d349a1a19450485ba2
emersonteles <emersonteles21@gmail.com>
2022-02-13 13:42:14 -0300

    feat: creating new product page

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 
-	"github.com/EmersonTeles/golang_Store/models"
+	"github.com/EmersonTeles/golang_Store/routes"
 )
 
-var temp = template.Must(template.ParseGlob ("templates/*.html"))
-
 func main(){
-	http.HandleFunc("/", index)
+	routes.LoadRoutes()
 	http.ListenAndServe(":8000", nil)
-}
-func index(w http.ResponseWriter, r *http.Request){
-	products := models.SearchProducts()
-	temp.ExecuteTemplate(w, "Index", products)
 }
